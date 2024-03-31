1b1ed3ae721e325173c1dfdff33ce704785938e1
shijo thomas <shijothomashome@gmail.com>
2024-03-31 19:06:44 +0530

    updated

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 export const MENU_IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
 
